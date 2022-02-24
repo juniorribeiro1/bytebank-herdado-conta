c2b1ac79033915b22f45e24aa6706ff87c9603db
Jose Roberto Ribeiro Junior - Desktop ADM <adm.jjunior@FUNARTE.CORP>
2022-02-24 13:55:09 -0300

    first commit

---
+++ src/br/com/bytebank/banco/test/util/TesteArrayListEquals.java
@@ -0,0 +1,30 @@
+package br.com.bytebank.banco.test.util;
+
+import java.util.ArrayList;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
+
+public class TesteArrayListEquals {
+
+	public static void main(String[] args) {
+
+		ArrayList<Conta> lista = new ArrayList<Conta>();
+
+		Conta cc1 = new ContaCorrente(22, 22);
+		Conta cc2 = new ContaPoupanca(22, 22);
+
+		lista.add(cc1);
+		
+		
+		 boolean existe = lista.contains(cc2);
+		
+		System.out.println("Ja existe? " + existe); 
+
+		for (Conta conta : lista) {
+			System.out.println(conta);
+		}
+
+	}
+}
